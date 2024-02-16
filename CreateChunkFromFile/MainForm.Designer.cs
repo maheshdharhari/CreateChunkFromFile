9789cac96a1309d789d350baff296bf457761f77
Mahesh <contact@maheshyadav.com.np>
2024-02-16 17:10:06 +0545

    UI size increased

--- CreateChunkFromFile/MainForm.Designer.cs
+++ CreateChunkFromFile/MainForm.Designer.cs
@@ -115,7 +115,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(581, 99);
+            this.ClientSize = new System.Drawing.Size(581, 110);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.textBox3);
